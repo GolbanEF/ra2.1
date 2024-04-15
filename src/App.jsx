7c79afbe02aba4355db6b1dd3a223ab7d21cbfe5
GolbanEF <Golban.ef@yandex.ru>
2024-04-15 19:46:05 +0300

    go

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+import Portfolio from './Portfolio';
+
+function App() {
+  return (
+  <div className='portfolio'>
+    <Portfolio />
+  </div>
+  );
+}
+
+export default App;
